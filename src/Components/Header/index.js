3044b1da8371955636dfa63c0b3f0b770994ec76
Jeroen Schaftenaar <4943036+jschaftenaar@users.noreply.github.com>
2019-04-01 19:04:34 -0500

    feat/v2: header with logo

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -3,7 +3,14 @@ import style from './Header.module.scss';
 
 const Header = () => (
   <div className={style.header}>
-    <h1 className={style.title}>Stock Sizer</h1>
+    <h1 className={style.title}>
+      <span class="fa-stack">
+        <i className={['fas', 'fa-circle', 'fa-stack-1x', style.icon_back].join(' ')}></i>
+        <i className={['fas', 'fa-circle', 'fa-stack-1x', style.icon_middle].join(' ')}></i>
+        <i className={['fas','fa-random','fa-stack-1x',style.icon_front].join(' ')}></i>
+      </span>
+      Stock Sizer
+    </h1>
   </div>
 );
 

a598a29fc2d53c3cd044ad493f1ed98b2c0b6841
Jeroen Schaftenaar <jschaft@us.ibm.com>
2020-04-14 23:36:57 -0500

    chmod .ssh directory and content

--- dokkudeploy.sh
+++ dokkudeploy.sh
@@ -11,6 +11,7 @@ if [ "$1" == "refs/heads/release/v1" ]; then
   mkdir -p ~/.ssh
   echo "$ID_RSA" > ~/.ssh/id_rsa
   echo "$ID_RSA_PUB" > ~/.ssh/id_rsa.pub
+  chmod 700 -R ~/.ssh
   ssh-keyscan -t rsa host.416.amsdfw.us >> ~/.ssh/known_hosts
   git remote add dokku dokku@host.416.amsdfw.us:stocksizer 
   git push dokku release/v1:master

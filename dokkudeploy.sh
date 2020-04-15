3ff0f2ce3573c9397142bdd65957d0ad0dab15e7
Jeroen Schaftenaar <4943036+jschaftenaar@users.noreply.github.com>
2020-04-15 00:41:34 -0500

    unshallow git repo

--- dokkudeploy.sh
+++ dokkudeploy.sh
@@ -8,6 +8,7 @@ if [ "$1" == "refs/heads/release/v1" ]; then
   echo "$ID_RSA" > ~/.ssh/id_rsa
   echo "$ID_RSA_PUB" > ~/.ssh/id_rsa.pub
   chmod 700 -R ~/.ssh
+  git fetch --unshallow
   ssh-keyscan -t rsa host.416.amsdfw.us >> ~/.ssh/known_hosts
   git remote add dokku dokku@host.416.amsdfw.us:stocksizer
   git push dokku release/v1:master --force

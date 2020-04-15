df79f622a4f8ee522711d25e2b3a4db9db9b6a15
Jeroen Schaftenaar <4943036+jschaftenaar@users.noreply.github.com>
2020-04-14 20:58:08 -0500

    add ssh-add command to deploy step

--- dokkudeploy.sh
+++ dokkudeploy.sh
@@ -8,8 +8,9 @@ echo "$1"
 if [ "$1" == "refs/heads/release/v1" ]; then
   mkdir -p ~/.ssh
   cp .sshconfig ~/.ssh/config
-  echo "$ID_RSA" >> ~/.ssh/id_rsa
-  echo "$ID_RSA_PUB" >> ~/.ssh/id_rsa.pub
+  echo "$ID_RSA" | tr -d '\r' | ssh-add -
+  #echo "$ID_RSA" >> ~/.ssh/id_rsa
+  #echo "$ID_RSA_PUB" >> ~/.ssh/id_rsa.pub
   git remote add dokku dokku@host.416.amsdfw.us:stocksizer 
   git push dokku release/v1:master
 fi

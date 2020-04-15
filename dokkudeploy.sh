26c84f380588b27d0f82a605f6cb14eec60269a6
Jeroen Schaftenaar <4943036+jschaftenaar@users.noreply.github.com>
2020-04-14 20:30:39 -0500

    correct deployed branch

--- dokkudeploy.sh
+++ dokkudeploy.sh
@@ -7,10 +7,9 @@ echo "$1"
 
 if [ "$1" == "refs/heads/release/v1" ]; then
   mkdir -p ~/.ssh
-  #cp .sshconfig ~/.ssh/config
-  echo "$2" >> ~/.ssh/id_rsa.pub
-  git clone --single-branch --branch release/v1 git@github.com:jschaftenaar/stocksizer.com.git
-  cd stocksizer
+  cp .sshconfig ~/.ssh/config
+  echo $2 >> ~/.ssh/id_rsa.stocksizer
   git remote add dokku dokku@host.416.amsdfw.us:stocksizer 
-  git push dokku master
+  git status
+  git push dokku release/v1:master
 fi

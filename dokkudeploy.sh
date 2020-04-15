b0ee9e2fe3789f39dd7f29fced9bfb077f9f6a05
Jeroen Schaftenaar <4943036+jschaftenaar@users.noreply.github.com>
2020-04-14 20:46:16 -0500

    correct environment variable handling

--- dokkudeploy.sh
+++ dokkudeploy.sh
@@ -8,7 +8,8 @@ echo "$1"
 if [ "$1" == "refs/heads/release/v1" ]; then
   mkdir -p ~/.ssh
   cp .sshconfig ~/.ssh/config
-  echo $2 >> ~/.ssh/id_rsa.stocksizer
+  echo "$ID_RSA" >> ~/.ssh/id_rsa
+  echo "$ID_RSA_PUB" >> ~/.ssh/id_rsa.pub
   git remote add dokku dokku@host.416.amsdfw.us:stocksizer 
   git push dokku release/v1:master
 fi

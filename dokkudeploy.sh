3c209b734bc25aa17632678025ab1067b41a59ce
Jeroen Schaftenaar <jschaft@us.ibm.com>
2020-04-14 23:51:04 -0500

    Deploy on release/v1 branch only, no force

--- dokkudeploy.sh
+++ dokkudeploy.sh
@@ -3,13 +3,12 @@
 echo "The branch is:"
 echo "$1"
 
-#if [ "$1" == "refs/heads/release/v1" ]; then
-if [ "$1" == "refs/heads/master" ]; then
+if [ "$1" == "refs/heads/release/v1" ]; then
   mkdir -p ~/.ssh
   echo "$ID_RSA" > ~/.ssh/id_rsa
   echo "$ID_RSA_PUB" > ~/.ssh/id_rsa.pub
   chmod 700 -R ~/.ssh
   ssh-keyscan -t rsa host.416.amsdfw.us >> ~/.ssh/known_hosts
   git remote add dokku dokku@host.416.amsdfw.us:stocksizer
-  git push dokku master --force
+  git push dokku release/v1:master
 fi

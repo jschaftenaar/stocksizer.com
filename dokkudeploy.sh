7fd3371fa04c22e0922ac2bde957fc5045e6a272
Jeroen Schaftenaar <jschaft@us.ibm.com>
2020-04-14 23:56:57 -0500

    force push git deploy

--- dokkudeploy.sh
+++ dokkudeploy.sh
@@ -10,5 +10,5 @@ if [ "$1" == "refs/heads/release/v1" ]; then
   chmod 700 -R ~/.ssh
   ssh-keyscan -t rsa host.416.amsdfw.us >> ~/.ssh/known_hosts
   git remote add dokku dokku@host.416.amsdfw.us:stocksizer
-  git push dokku release/v1:master
+  git push dokku release/v1:master --force
 fi

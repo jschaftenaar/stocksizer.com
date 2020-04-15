7a5b2206abd9878db662584f362bbf0692d428c5
Jeroen Schaftenaar <jschaft@us.ibm.com>
2020-04-14 23:42:50 -0500

    correctly force push, attempt on master

--- dokkudeploy.sh
+++ dokkudeploy.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
 
-ls -la
-
 echo "The branch is:"
 echo "$1"
 
-if [ "$1" == "refs/heads/release/v1" ]; then
-  #cp .sshconfig ~/.ssh/config
-  #echo "$ID_RSA" | tr -d '\r' | ssh-add -
+#if [ "$1" == "refs/heads/release/v1" ]; then
+if [ "$1" == "master" ]; then
   mkdir -p ~/.ssh
   echo "$ID_RSA" > ~/.ssh/id_rsa
   echo "$ID_RSA_PUB" > ~/.ssh/id_rsa.pub
   chmod 700 -R ~/.ssh
   ssh-keyscan -t rsa host.416.amsdfw.us >> ~/.ssh/known_hosts
-  git remote add dokku dokku@host.416.amsdfw.us:stocksizer --force
-  git push dokku release/v1:master
+  git remote add dokku dokku@host.416.amsdfw.us:stocksizer
+  git push dokku release/v1:master --force
 fi

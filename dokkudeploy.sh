3483073bc6ddd7998ce64665857dbf97d21988d0
Jeroen Schaftenaar <jschaft@us.ibm.com>
2020-04-14 19:07:53 -0500

    Correct reference, add mkdir

--- dokkudeploy.sh
+++ dokkudeploy.sh
@@ -4,6 +4,7 @@ echo "The branch is:"
 echo "$1"
 
 if [ "$1" == "refs/heads/release/v1" ]; then
+  mkdir -p ~/.ssh
   cp .sshconfig ~/.ssh/config
   echo "$2" >> ~/.ssh/id_rsa.stocksizer
   git remote add dokku dokku@host.416.amsdfw.us:stocksizer
